19a31f544d6a39d68e29ab6090fe5409bd42d0f1
thecosmeticdesigner <thecosmeticdesigner@gmail.com>
2023-01-25 07:05:35 -0600

    message

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
+
 import validator from './validator.js';
 
-console.log(validator);
+document.getElementById("inputNumero").addEventListener("keypress", valideKey)
+document.getElementById("validartarjeta").addEventListener("click", validate);
+
+function validate() {
+  const result = document.getElementById("result");
+  const valid = validator.isValid(document.getElementById("inputNumero").value)
+  if (valid) {
+    result.innerHTML = "Tarjeta Valida \n" + validator.maskify(document.getElementById("inputNumero").value)
+    //Show success in div#result
+  }
+  else {
+    result.innerHTML = "Tarjeta invalida"
+    // Show error message in div#result
+  }
+
+}
+function valideKey(evt) {
+
+  // code is the decimal ASCII representation of the pressed key.
+  const code = (evt.which) ? evt.which : evt.keyCode;
+ 
+  if (code === 8) { // backspace.
+    return true;
+  } else if (code >= 48 && code <= 57) { // is a number.
+    return true;
+  } else { 
+    evt.preventDefault()    //input only numbers addEventListener. 
+    return false;
+  }
+
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,67 @@
 const validator = {
-  // ...
+  isValid: isValid,
+  maskify: maskify
+
 };
 
 export default validator;
+
+
+function isValid(creditCardNumber) {
+  const procesado = creditCardNumber.trim()
+  if (procesado.length === 0) {
+    return false
+
+  }
+  const arrayNumeroTarjeta = procesado.split("")
+
+  const nuevoArrayInvertido = arrayNumeroTarjeta.reverse();
+
+  const arrayPar = nuevoArrayInvertido.filter(function (x, y) {
+    if (y % 2 === 1) return x;
+  });
+
+  let arrayImpar = nuevoArrayInvertido.filter(function (x, y) {
+    if (y % 2 === 0) return x;
+  });
+
+  const arrayplus = arrayPar.map(function (parmulti) {
+    const multipar = parmulti * 2;
+
+
+    if (multipar >= 10) {
+      const diezmayor = multipar.toString().split("").map(Number); 
+
+      const arraytres = diezmayor.reduce(function (x, z) {
+        return x + z;
+      });
+
+      return arraytres;
+    }
+    else { return multipar }
+  });
+  arrayImpar = arrayImpar.map(Number)
+
+  const sumaplus = arrayplus.reduce(function (x, y) {
+    return x + y
+  })
+  const sumaImpar = arrayImpar.reduce(function (x, z) {
+    return x + z;
+  })
+  const resultado = sumaplus + sumaImpar;
+
+  if (resultado % 10 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+function maskify(cc) {
+  return cc.slice(0, -4).replace(/./g, '#') + cc.slice(-4);
+
+
+
+}
+
